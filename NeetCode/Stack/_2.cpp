152a01979c9884c15910a93b5e72eec418f8daef
k3uual <kewaltailor345@gmail.com>
2024-02-11 00:53:39 +0530

    Neetcode-stack-2

--- NeetCode/Stack/_2.cpp
+++ NeetCode/Stack/_2.cpp
@@ -6,21 +6,28 @@ using namespace std;
 
 class MinStack {
 public:
-    int min;
-    vector<int> stck;
+    vector<int> stck; 
+    vector<int> min;
 
     MinStack() {
+        cout<<"minstack\n";
         stck.clear();
-        min = NULL;
+        min.clear();
+        min.push_back(2147483647);
     }
     
     void push(int val) {
-        if(min == NULL || min >val);
-            min = val;
+        int x = min.size()-1;
+        if(min[x] >= val)
+            min.push_back(val);
         stck.push_back(val);
     }
     
     void pop() {
+        int x = stck[stck.size()-1];
+        int min_top = min[min.size()-1];
+        if(min_top == x)
+            min.pop_back();
         stck.pop_back();
     }
     
@@ -30,11 +37,49 @@ public:
     }
     
     int getMin() {
-        return min;
+        int x = min.size()-1;
+        if(x == 0){
+            cout<<"run";
+            return NULL;
+        }
+        return min.at(x);
     }
 };
 
 int main(){
     MinStack m;
-    m.top();
+    int choice, val;
+    // printf("Enter choice:\n");
+    // scanf("&%d",choice);
+    // while(choice < 5){
+    //     switch (choice){
+    //         case 1:
+    //             printf("enter data: ");
+    //             scanf("&%d",val);
+    //             m.push(val);
+    //             break;
+    //         case 2:
+    //             m.pop();
+    //             break;
+    //         case 3:
+    //             m.top();
+    //             break;
+    //         case 4:
+    //             m.getMin();
+    //             break;
+    //         default:
+    //             printf("exit");
+    //             break;
+    //     }
+    // }
+    m.push(-2);
+    m.push(0);
+    m.push(-3);
+    cout<<m.getMin();
+    m.pop();
+    cout<<m.top();
+    cout<<m.getMin();
+    m.pop();
+    m.pop();
+    cout<<m.getMin();
 }
