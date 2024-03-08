33edf6f7dbaec732c2911f170fa4a8ccd2306304
k3uual <kewaltailor345@gmail.com>
2024-03-08 17:13:02 +0530

    Explore better solution

--- NeetCode/Array_Hashing/_2alt.cpp
+++ NeetCode/Array_Hashing/_2alt.cpp
@@ -6,27 +6,29 @@ using namespace std;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int i;
+        int arr[26] = {0};
         int size = s.size();
-        if(t.size() != s.size())
+        if(size != t.size()) {
             return false;
-        unordered_map<char, int> track;
-        
-        for(i = 0; i < size; i++) {
-                track[s[i]]++;
-                track[t[i]]--;
         }
 
-        for(auto t : track) {
-            if(t.second != 0)
-                return false;
+        int i;
+        for(i = 0; i < size; i++) {
+            arr[s[i] - 97]++;
         }
+
+        for(i = 0; i < size; i++) {
+            arr[t[i] - 97]--;
+            if(arr[t[i] - 97] < 0){
+                return false;
+            }
+        }
+
         return true;
-        
     }
 };
 
 int main(){
     Solution sol;
-    cout<<sol.isAnagram("anagram","gramana");
+    cout << sol.isAnagram("anagram","gramana");
 }
