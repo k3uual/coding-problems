40392772234943fa6fef7e4b9bb66e4f941ca7b6
k3uual <kewaltailor345@gmail.com>
2024-03-08 15:45:45 +0530

    Explored better solution

--- NeetCode/Array_Hashing/_1.cpp
+++ NeetCode/Array_Hashing/_1.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include<unordered_map>
 using namespace std;
 
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1])
+        unordered_map<int, int> track;
+        for(int num : nums) {
+            if(track[num] == 1) {
                 return true;
+            }
+            track[num]++;
         }
         return false;
     }
@@ -17,6 +20,6 @@ public:
 
 int main(){
     Solution sol;
-    vector<int> num={1,2,3,4,6,1};
+    vector<int> num={1,2,3,4,6,15,1};
     cout<<sol.containsDuplicate(num);
 }
