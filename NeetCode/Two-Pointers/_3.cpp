0b3c63eb858fa03284b7285d1fe12a38fdd64858
k3uual <kewaltailor345@gmail.com>
2024-03-14 23:48:20 +0530

    Try to solve

--- NeetCode/Two-Pointers/_3.cpp
+++ NeetCode/Two-Pointers/_3.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
 class Solution {
@@ -9,21 +10,39 @@ public:
         int count = 0;
         int n = nums.size();
         
-        for(int i = 0; i < n; i++) {
-            //cout << sum << endl;
-            for(int j = 0; j < n; j++) {
-                if(i == j)
-                    continue;
-                int sum = nums[i] + nums[j] + nums[j+1];
-                if(sum == 0){
-                    sol.push_back(std::vector<int>());
-                    sol[count].push_back(nums[i]);
-                    sol[count].push_back(nums[i + 1]);
-                    sol[count].push_back(nums[j]);
-                    cout << nums[i] << nums[i + 1] << nums[j] << endl;
-                    count++;
-                }
+        // for(int i = 0; i < n; i++) {
+        //     //cout << sum << endl;
+        //     for(int j = 0; j < n; j++) {
+        //         if(i == j)
+        //             continue;
+        //         int sum = nums[i] + nums[j] + nums[j+1];
+        //         if(sum == 0){
+        //             sol.push_back(std::vector<int>());
+        //             sol[count].push_back(nums[i]);
+        //             sol[count].push_back(nums[i + 1]);
+        //             sol[count].push_back(nums[j]);
+        //             cout << nums[i] << nums[i + 1] << nums[j] << endl;
+        //             count++;
+        //         }
+        //     }
+        // }
+        unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); i++) {
+            map[nums[i]] = i;
+        }
+
+        for(int i = 0; i < nums.size() - 1; i=i+2) {
+            int target = 0 - (nums[i] + nums[i+1]);
+            cout << target << endl;
+            if(map.at(target) != i) {
+                sol.push_back(std::vector<int>());
+                sol[count].push_back(nums[i]);
+                sol[count].push_back(nums[i + 1]);
+                sol[count].push_back(target);
+                cout << nums[i] << nums[i + 1] << target << endl;
+                count++;
             }
+            
         }
         return sol;
     }
@@ -35,8 +54,8 @@ int main() {
     vector<vector<int>> sol = s.threeSum(num);
     
     for(int i = 0; i < sol.size(); i++) {
-        for(int j = i+1; j < sol[i].size(); i++) {
-            cout << sol[i][j] << endl;
+        for(int j = i+1; j < 3; i++) {
+            cout << sol[i][j] << "  ";
         }
         cout << "----" << endl;
     }
