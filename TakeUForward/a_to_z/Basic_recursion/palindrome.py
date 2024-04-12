cb99ec6e38ff32f7caad3ed88a393de692936601
k3uual <kewaltailor345@gmail.com>
2024-04-12 17:46:55 +0530

    Completed recursion

--- TakeUForward/a_to_z/Basic_recursion/palindrome.py
+++ TakeUForward/a_to_z/Basic_recursion/palindrome.py
@@ -1,4 +1,10 @@
-num = 123321
+s = "123321"
 
-def palindrome(n):
-    pass
+def palindrome(i, n):
+    if(i > n):
+        return True
+    if(s[i] != s[n]):
+        return False
+    return palindrome(i + 1, n - 1)
+
+print(palindrome(0, len(s) - 1))
