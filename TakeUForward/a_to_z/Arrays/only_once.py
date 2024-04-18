52a78465c0372c3a25d410973bcdec0d8f4b5904
k3uual <kewaltailor345@gmail.com>
2024-04-18 10:37:55 +0530

    Arr

--- TakeUForward/a_to_z/Arrays/only_once.py
+++ TakeUForward/a_to_z/Arrays/only_once.py
@@ -2,13 +2,14 @@
 def onlyOnce(a):
     a.sort()
     once = 0
-    for i in range(0,len(a)-1,2):
-        if a[i] != a[i+1]:
-            return a[i+1]
+    for i in range(1,len(a),2):
+        print(a[i],a[i-1])
+        if a[i] != a[i-1]:
+            return a[i-1]
     
     if(i+1 < len(a)):
-        once = a[i+1]
+        return a[i+1]
     
     return -1
 
-print(onlyOnce([1,2,2]))
+print(onlyOnce([1,1,2,3,3]))
