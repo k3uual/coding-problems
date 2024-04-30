ef6b3d0d763729661e08d1947341007065f9a37a
k3uual <kewaltailor345@gmail.com>
2024-04-30 23:13:23 +0530

    Array Ques

--- TakeUForward/a_to_z/Arrays/rearrange_pos_neg.py
+++ TakeUForward/a_to_z/Arrays/rearrange_pos_neg.py
@@ -1,26 +1,19 @@
 
 def rearrange(a):
-    i,j = 0,1
-    while(i <= j and j < len(a)):
-        if(i%2 == 1 and a[i] > 0):
-            print(a[i], a[j],i,j,"first")
-            if(a[j] < 0):
-                a[i], a[j] = a[j], a[i]
-                print("swap frst", a[i], a[j],i,j)
-                i += 1
-                j += 1
-        elif(i%2 == 0 and a[i] < 0):
-            print(a[i], a[j],i,j,"sec")
-            if(a[j] > 0):
-                a[i], a[j] = a[j], a[i]
-                print("swap sec", a[i], a[j],i,j)
-                j += 1
-                i += 1
+    new = [0] * len(a)
+
+    posInd, negInd = 0, 1
+
+    for i in range(len(a)):
+        if(a[i] < 0):
+            new[negInd] = a[i]
+            negInd += 2
+
         else:
-            i += 1
-            print("ok", i)
-        j += 1    
-                    
-a = [1,2,3,-1,-2,-3]    
-rearrange(a)        
-print(a)            
+            new[posInd] = a[i]
+            posInd += 2
+    
+    return new
+
+a = [1, 2, 3, -1, -2, -3, -4, -5, 2, 1]    
+print(*(rearrange(a)))
