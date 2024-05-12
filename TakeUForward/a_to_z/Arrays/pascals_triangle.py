34fe7e3152993f8cb7b0d5ed7627e43b024cd585
k3uual <kewaltailor345@gmail.com>
2024-05-12 12:22:28 +0530

    Pascal's Triangle

--- TakeUForward/a_to_z/Arrays/pascals_triangle.py
+++ TakeUForward/a_to_z/Arrays/pascals_triangle.py
@@ -1,4 +1,34 @@
 
-def pascal(n):
-    a = [][]
-    count 
+def nCr(r, c):
+    # (r-1) 
+    #     C 
+    #     (c-1)
+    ans = 1
+    for i in range(1, c):
+        ans *= (r - i)
+        ans /= i
+
+    return int(ans)
+def row(n):
+    a = [1]
+    ans = 1
+    for i in range(1, n):
+        ans *= (n - i)
+        ans /= i
+        a.append(int(ans))
+    
+    return a
+    
+def col(n):
+    ans_col = []
+    for i in range(n + 1):
+        ans_col.append(row(i))
+    return ans_col
+
+print(nCr(5, 3)) # Prints element present at that particular row and column
+print("----------")
+print(*row(5))   # Prints entire row of the triangle
+print("----------")
+ans = col(5)     # Prints entire Pascal's Triangle
+for i in range(len(ans)):
+    print(*ans[i])
