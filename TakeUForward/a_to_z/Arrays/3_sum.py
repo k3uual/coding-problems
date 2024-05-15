99bec1929bc45df2478e4f19cfb7b0aae7b8bb1e
k3uual <kewaltailor345@gmail.com>
2024-05-15 15:51:23 +0530

    Arrays

--- TakeUForward/a_to_z/Arrays/3_sum.py
+++ TakeUForward/a_to_z/Arrays/3_sum.py
@@ -1,35 +1,34 @@
 
+def update_first(i, a, n, j = -1):
+    while(a[i] == a[i+1] and i < n):
+        i += 1
+    return(i)
+
+def update_end(k, a):
+    while(a[k] == a[k-1]):
+        k -= 1
+    return(k - 1)
+
 def _3sum(a):
     a.sort()
-    i, j, k = 0, len(a)-1, 1
-    flag = 0
+    i = -1
     ans = []
-    
-    while(i < j):
-        temp = 0 - (a[i] + a[j])
-    
-        if(a[k] < temp and k < j - 1):
-            k += 1
-        else:
-            print(i, j, k, temp)
-            if(a[k] == temp):
-                print("---", i, j, k, temp)
-                ans.append([a[i], a[k], a[j]])
-            if(flag % 2):
-                i += 1
-                # x = i + 1
-                # while(a[i] == a[x]):
-                #     i += 1
-                #     x += 1
-            else:
-                # j -= 1
-                # x = j - 1
-                # while(a[j] == a[x]):
-                j -= 1
-            k = i + 1
-        flag += 1
-            
+    while(i < len(a) - 2):
+        i += 1
+        j = i + 1
+        k = len(a) - 1
+        while(j < k):
+            sm = a[i] + a[j] + a[k]
+            if(sm == 0):
+                ans.append([a[i], a[j], a[k]])
+                j = update_first(j, a, len(a)-1) + 1
+                k = update_end(k, a)
+            if(sm > 0):
+                k = update_end(k, a)
+            elif(sm < 0):
+                j = update_first(j, a, len(a)-1) + 1
+        i = update_first(i, a, len(a)-2)
+        
     return ans
 
-print(_3sum([-1,0,1,2,-1,-4]))
-# -4, -1, -1, 0, 1, 2
+print(_3sum([-2, -2, -2, -1, -1, -1, 0, 0, 0, 2, 2, 2, 2]))
