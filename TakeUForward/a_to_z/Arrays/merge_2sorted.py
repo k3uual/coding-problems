7231ac085c9a61892b6285569dd7fd07c5a1c10a
k3uual <kewaltailor345@gmail.com>
2024-05-28 08:40:09 +0530

    arr

--- TakeUForward/a_to_z/Arrays/merge_2sorted.py
+++ TakeUForward/a_to_z/Arrays/merge_2sorted.py
@@ -1,32 +1,32 @@
 from typing import List
+import math
+class Solution:
+    def __init__(self):
+        pass
 
-def merge_2sorted(a:List[int], b:List[int])-> List[int]:
-    sort = []
-    i, j = 0, 0
-    while(i < len(a) and j < len(b)):
-        if(a[i] < b[j]):
-            sort.append(a[i])
-            i += 1
-        else:
-            sort.append(b[j])
-            j += 1
-    
-    for i in range(len(a)):
-        sort.append(a[i])
-    for j in range(len(b)):
-        sort.append(b[j])
-    
-    i = 0
-    while(i < len(sort)):
-        for j in range(0, len(a)):
-            a[j] = sort[i]
-            print(a)
-            i += 1
-        for k in range(0, len(b)):
-            b[k] = sort[i]
-            i += 1
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        gap = m + n
+        while(gap > 1):
+            gap = (gap//2) + (gap%2)
+            left = 0
+            right = left + gap
 
-arr1 = [1,4,8,10] 
-arr2 = [2,3,9]
-merge_2sorted(arr1, arr2)
+            while(right < m+n):
+                if(right >= m and left < m):
+                    self.swap_if_great(nums1, left, nums2, right - m)
+                elif(left >= m):
+                    self.swap_if_great(nums2, left - m, nums2, right - m)
+                else:
+                    self.swap_if_great(nums1, left, nums1, right)
+                left += 1
+                right += 1
+    
+    def swap_if_great(self, nums1: List[int], ind1: int, nums2: List[int], ind2: int):
+        if(nums1[ind1] > nums2[ind2]):
+            nums1[ind1], nums2[ind2] = nums2[ind2], nums1[ind1]
+                    
+arr1 = [1,3,5,7] 
+arr2 = [0,2,6,8,9]
+sol = Solution()
+sol.merge(arr1, len(arr1), arr2, len(arr2))
 print(arr1, arr2)
