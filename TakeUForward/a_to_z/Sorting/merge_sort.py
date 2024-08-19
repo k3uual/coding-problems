47c0aa75dbbae5431c58b470b6fb59566e54a5ea
k3uual <kewaltailor345@gmail.com>
2024-08-19 19:11:00 +0530

    DSA array

--- TakeUForward/a_to_z/Sorting/merge_sort.py
+++ TakeUForward/a_to_z/Sorting/merge_sort.py
@@ -1,8 +1,39 @@
-def merge(a):
-    pass
+def merge(a, low, high):
+    if(low >= high): return
 
-def sort():
-    pass
+    mid = (low + high)//2
+    
+    merge(a, low, mid)
+    
+    merge(a, mid+1, high)
+    
+    sort(a, low, mid, high)
+
+def sort(a, low, mid, high):
+    temp = []
+    left = low
+    right = mid + 1
+    print(a[left:right])
+    while(left <= mid and right <= high):
+        if(a[right] < a[left]):
+            temp.append(a[right])
+            right += 1
+        else:
+            temp.append(a[left])
+            left += 1
+
+    while(left <= mid):
+        temp.append(a[left])
+        left += 1
+    while(right <= high):
+        temp.append(a[right])
+        right += 1
+    
+    for i in temp:
+        a[low] = i
+        low += 1
+    print(a, "high=", left, "low=", right)
 
 a = [4,2,1,6,7]
-merge(a)
+merge(a, 0, len(a)-1)
+# print(a)
