85096fe8f9830d8a51f58444f448e8b1d6a10ee0
k3uual <kewaltailor345@gmail.com>
2024-10-13 01:27:01 +0530

    String

--- TUF/a_to_z/Strings/Easy/isomorphic.py
+++ TUF/a_to_z/Strings/Easy/isomorphic.py
@@ -3,24 +3,24 @@ class Solution(object):
     def isIsomorphic(self, s, t):
         if(len(s) != len(t)):
             return False
-        mpS = [-1]*26
-        mpT = [-1]*26
+
+        ds = {}
+        dt = {}
 
         for i in range(len(s)):
-            sInd = ord(s[i]) - 97
-            tInd = ord(t[i]) - 97
-            if(mpS[sInd] != -1 or mpT[tInd] != -1):
-                if((mpS[sInd] != tInd) or (mpT[tInd] != sInd)):
+            if ((s[i] in ds) ^ (t[i] in dt)):
+                return False
+            elif not (ds.get(s[i])):
+                ds[s[i]] = t[i]
+                dt[t[i]] = s[i]
+            else:
+                if(ds.get(s[i]) != t[i] and dt.get(t[i]) != s[i]):
                     return False
-            mpS[sInd] = tInd
-            mpT[tInd] = sInd
         
         return True
 
-s = "eggfe"
-t = "addta"
-s = "bbbaaaba"
-t = "aaabbbba"
+s = "eggge"
+t = "addda"
 sol = Solution()
 res = sol.isIsomorphic(s, t)
 print(res)
